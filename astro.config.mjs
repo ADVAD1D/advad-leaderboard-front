7692a0b55f613c04374c3d56fdba79d4ce0a135e
ANGELUS11 <168485556+ANGELUSD11@users.noreply.github.com>
2026-04-11 11:20:55 -0500

    chore: update site URL and make base path conditional on GitHub Actions environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ANGELUSD11.github.io',
-  base: '/advad-leaderboard-front',
+  site: 'https://ADVAD1D.github.io',
+  // @ts-ignore: process.env is valid in Node during build time
+  ...(process.env.GITHUB_ACTIONS ? { base: '/advad-leaderboard-front' } : {}),
 });
